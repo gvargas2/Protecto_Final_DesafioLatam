040d32901c8cb794b7858b39b5e2d8e3be90f9d1
Gabriela <gvargas2@uc.cl>
2019-04-06 14:01:25 -0300

    15: Se elimina archivos de tooltipser y boxSlider. Se busca otro jquery plugin

--- wp-content/themes/tema_proyecto/assets/js/app.js
+++ wp-content/themes/tema_proyecto/assets/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   // Inicializando AOS
   AOS.init();
-  
+
   // ADD BOOTSTRAP CLASS FOR NAVBAR MENU
   $('#menu-mi_menu li').addClass('nav-item').children(this).addClass('nav-link js-scroll-trigger');
   $('.nav-item:last-of-type a').addClass('btn btn-primary').removeClass('js-scroll-trigger');
